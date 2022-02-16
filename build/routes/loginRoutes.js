928fdf1437ae6932eeb1ac9eee98724079f2ea4e
KronosGR <geo.elgeo@gmail.com>
2022-02-16 20:01:21 +0100

    check login status

--- build/routes/loginRoutes.js
+++ build/routes/loginRoutes.js
@@ -21,8 +21,29 @@ router.get('/login', (req, res) => {
 });
 router.post('/login', (req, res) => {
     const { email, password } = req.body;
-    if (email)
-        res.send(email.toUpperCase());
-    else
-        res.send('You must provide an email');
+    if (email && password && email === 'hi@hi.com' && password === '1234') {
+        req.session = { loggedIn: true };
+        res.redirect('/');
+    }
+    else {
+        res.send('Invalid email or password');
+    }
+});
+router.get('/', (req, res) => {
+    if (req.session && req.session.loggedIn) {
+        res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+    }
+    else {
+        res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+    }
 });
--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -1,4 +1,4 @@
-import { Request, Response, Router } from 'express';
+import { Request, response, Response, Router } from 'express';
 
 interface RequestWithBody extends Request {
   body: { [key: string]: string | undefined };
@@ -25,8 +25,30 @@ router.get('/login', (req: Request, res: Response) => {
 router.post('/login', (req: RequestWithBody, res: Response) => {
   const { email, password } = req.body;
 
-  if (email) res.send(email.toUpperCase());
-  else res.send('You must provide an email');
+  if (email && password && email === 'hi@hi.com' && password === '1234') {
+    req.session = { loggedIn: true };
+    res.redirect('/');
+  } else {
+    res.send('Invalid email or password');
+  }
+});
+
+router.get('/', (req: Request, res: Response) => {
+  if (req.session && req.session.loggedIn) {
+    res.send(`
+      <div>
+        <div>You are logged in</div>
+        <a href="/logout">Logout</a>
+      </div>
+    `);
+  } else {
+    res.send(`
+      <div>
+        <div>You are not logged in</div>
+        <a href="/login">Login</a>
+      </div>
+    `);
+  }
 });
 
 export { router };
