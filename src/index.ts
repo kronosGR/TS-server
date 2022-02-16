67c923957b62874df63f488a36edd34536fa2231
KronosGR <geo.elgeo@gmail.com>
2022-02-16 19:32:34 +0100

    added post f for login endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import bodyParser from 'body-parser';
 import express, { Request, Response } from 'express';
-import { router } from '../routes/loginRoutes';
+import { router } from './routes/loginRoutes';
 
 const app = express();
 
-app.use(router)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
