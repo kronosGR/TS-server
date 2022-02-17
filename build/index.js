d25501c149b0b1039dd64e8fef52fbd92fcc0f34
KronosGR <geo.elgeo@gmail.com>
2022-02-17 09:26:21 +0100

    a lot of fixes

--- build/index.js
+++ build/index.js
@@ -5,12 +5,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_session_1 = __importDefault(require("cookie-session"));
-const express_1 = __importDefault(require("express"));
+'express';
+const AppRouter_1 = require("./AppRouter");
 const loginRoutes_1 = require("./routes/loginRoutes");
-const app = (0, express_1.default)();
+const app = express();
 app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_session_1.default)({ keys: ['kronos'] }));
 app.use(loginRoutes_1.router);
+app.use(AppRouter_1.AppRouter.getInstance());
 app.listen(3000, () => {
     console.log('Listening on port 3000');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import bodyParser from 'body-parser';
 import cookieSession from 'cookie-session';
-import express, { Request, Response } from 'express';
+import expressfrom 'express';
+import { AppRouter } from './AppRouter';
 import { router } from './routes/loginRoutes';
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cookieSession({keys:['kronos']}))
+app.use(cookieSession({ keys: ['kronos'] }));
 app.use(router);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
   console.log('Listening on port 3000');
